f977260fbd7322f82ab146b46d2ed2f188a202d3
Lasse <lasse96-06@hotmail.com>
2018-11-26 11:09:57 +0100

    asd

---
+++ src/FrontPage.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './CSS/FrontPage.css';
+
+class FrontPage extends Component {
+    render() {
+        return (
+            <div className="App">
+               <p>Hej</p>
+            </div>
+        );
+    }
+}
+
+export default FrontPage;
---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './CSS/index.css';
+import FrontPage from './FrontPage';
+
+ReactDOM.render(<FrontPage />, document.getElementById('root'));
